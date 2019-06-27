f026221233f3fb58279ae5f8ba48765c237c80dc
Norbert Kristof <norbulate@gmail.com>
2019-06-27 10:22:01 +0200

    wohhooo2

---
+++ week-01/day-4/sample.js
@@ -0,0 +1,2 @@
+console.log;
+"Printing on the screen...";
---
+++ week-01/day-4/sample.ts
@@ -0,0 +1,13 @@
+const pi: number = 3.14;
+let times: number = 12;
+let myName: string = "Norbs";
+
+let itCanbeUndefined: number;
+console.log(itCanbeUndefined);
+
+console.log("Printing on the screen...");
+console.log(times);
+console.log(myName);
+console.log(true);
+console.log(80 * 2);
+console.log(1.333333);
